3eaf684955aeaea5851ee7e798556ffad3fa1506
Christian Moya <cmoyacharro@gamil.com>
2023-06-26 10:45:01 -0500

    manejo de modulos

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,35 @@
 <template>
-  <CaracolaMagica/>
+  
+  
+  <CaracolaMagica />
 </template>
 
 
 <script>
-  import CaracolaMagica from './components/CaracolaMagica.vue';
+import CaracolaMagica from './components/CaracolaMagica.vue';
+
+import PokemonPage from './modules/components/pages/PokemonPage.vue';
+import Calculadora from './components/Calculadora.vue';
+
+
+
 export default {
   name: 'App',
   components: {
+    //Calculadora
     CaracolaMagica
+    
   }
 }
 </script>
 
 <style>
- #app {
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  /* color: #2c3e50; */
+  color: #2c3e50; 
   margin-top: 60px;
-} 
+}
 </style>
---
+++ src/modules/components/pages/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Juego Pokemon</h1>
+    <PokemonImg />
+    <PokemonOps />
+</template>
+
+<script>
+import Pokemonimg from '../Pokemonimg.vue'
+import PokemonOps from '../PokemonOps.vue'
+
+export default {
+    components: {
+        Pokemonimg,
+        PokemonOps
+    }
+
+}
+</script>
+
+<style></style>
